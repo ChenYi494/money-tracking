0c71fc1192ca44329dbc89ff7d8064fbbf2cbed5
lexie <lexiechen494@gmail.com>
2025-04-04 21:15:02 +0800

    feat: 新增&刪除分類API

--- server/lib/category.py
+++ server/lib/category.py
@@ -9,6 +9,7 @@ from ..utils import responses as resp
 from ..utils.responses import response_with
 # from ..utils.sql_build import sql_insert, sql_update, sql_delete, sql_select
 from ..utils.database import db
+from server.model.category import Category
 
 
 # 取得所有分類
@@ -19,11 +20,52 @@ def category_info():
 
 # 新增一筆分類
 def create_category(request):
-    data = request.get_json()
-    return response_with(resp.SUCCESS_200, value={"data": "新增成功"})
+    try:
+        data = request.get_json()
+
+        # 檢查必填欄位
+        if not data.get('type') or not data.get('name'):
+            return response_with(resp.BAD_REQUEST_400, value={"data": '參數缺失'})
+
+        # 建立新的分類資料
+        new_data = Category(
+            type=data.get('type'),
+            name=data.get('name')
+        )
+
+        # 寫入資料庫
+        db.session.add(new_data)
+        db.session.commit()
+
+        return response_with(resp.SUCCESS_200, value={"data": '成功新增一筆分類'})
+
+    except Exception as e:
+        db.session.rollback()
+        return response_with(resp.SERVER_ERROR_500, value={"data": '新增失敗，請重新操作'})
 
 
 # 刪除一筆分類
 def delete_category(request):
-    data = request.get_json()
-    return response_with(resp.SUCCESS_200, value={"data": "刪除成功"})
+    try:
+        data = request.get_json()
+        category_id = data.get('id')
+
+        # 查找該筆資料
+        category = db.session.get(Category, category_id)
+
+        if not category:
+            return response_with(resp.SERVER_ERROR_404, value={"data": '該分類不存在'})
+
+        # 確保category屬於當前session
+        category = db.session.merge(category)
+
+        # 刪除該筆資料
+        db.session.delete(category)
+        db.session.commit()
+
+        return response_with(resp.SUCCESS_200, value={"data": '成功刪除一筆分類'})
+
+    except Exception as e:
+        db.session.rollback()
+        return response_with(resp.SERVER_ERROR_500, value={"data": '刪除失敗，請重新操作'})
+
--- server/model/category.py
+++ server/model/category.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 # 收支種類資料表
 class Category(db.Model):
     __tablename__ = 'category'
-    id = db.Column(db.String(50), primary_key=True, comment='編號')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, comment='編號')
     type = db.Column(db.String(20), comment='種類')
     name = db.Column(db.String(20), comment='分類名稱')
 
