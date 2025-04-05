af60496ad9462f3c3e12b1e3bee265003cadc11a
lexie <lexiechen494@gmail.com>
2025-04-05 22:46:38 +0800

    fix: 修正model欄位

--- server/model/users.py
+++ server/model/users.py
@@ -8,3 +8,4 @@ class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.String(50), primary_key=True, comment='編號')
     name = db.Column(db.String(20), comment='名稱')
+    password = db.Column(db.String(100), comment='密碼')
